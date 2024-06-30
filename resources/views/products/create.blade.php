814b05bf5112a0f260584827230d96cb7c1cdd02
saim-x <x_aci@hotmail.com>
2024-06-30 15:59:53 +0500

    n

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,31 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel CRUD App</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-sm bg-dark">
+    <nav class="navbar navbar-expand-sm bg-dark">
 
-<div class="container-fluid">
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link text-light" href="/">Products</a>
-    </li>
-    
-  </ul>
-</div>
+        <div class="container-fluid">
+            <!-- Links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="/">Products</a>
+                </li>
 
-</nav>
+            </ul>
+        </div>
+
+    </nav>
     <div class="container">
-        <h1>
-            Create a new Product
-        </h1>
+        <div class="row justify-content-center">
+
+            <div class="col-sm-8">
+                <div class="card mt-3 p-3">
+                    <form action="/products/store" method="POST">
+                        @csrf
+
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Product Name</label>
+                            <input type="text" class="form-control" id="name" name="name" required>
+                        </div>
+
+
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Description</label>
+                            <textarea class="form-control" id="description" name="description" required></textarea>
+                        </div>
+
+                        <div class="mb-3">
+                            <label>Image</label>
+                            <input type="file" name="image" class="form-control" required>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Create Product</button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
     </div>
 </body>
+
 </html>
